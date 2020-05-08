f17061c0c8fc14882287c4bde19eb22d8845025e
Yoon <yoon.kim@brabbit.com>
2020-05-08 14:32:12 +0900

    ts해결

--- text-cook/vue.config.js
+++ text-cook/vue.config.js
@@ -1,5 +1,19 @@
+const webpack = require('webpack')
+// const ansiRegex = require('ansi-regex')
+
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
+  assetsDir: 'assets',
+  productionSourceMap: false,
+  publicPath: '',
+  transpileDependencies: [/node_modules[/\\\\]vuetify[/\\\\]/],
+  // transpileDependencies: ['vuetify'],
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery'
+      })
+    ]
+  }
 }
